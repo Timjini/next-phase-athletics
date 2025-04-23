15455dccbecc2d51d6ae057efa85219ff311b7d7
Timjini <hatim.jini@gmail.com>
2025-04-23 12:32:24 +0200

    update config

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,23 @@ import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@react-google-maps/api$': '<rootDir>/__mocks__/@react-google-maps/api.ts',
   },
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        // Enable JSX processing
+        babelConfig: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    ]
   },
   transformIgnorePatterns: [
     '/node_modules/(?!(@react-google-maps/api)/)',
