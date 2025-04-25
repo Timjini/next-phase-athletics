03b1bd6a7bd32f09393b69ed76c282c296bbac1b
Timjini <hatim.jini@gmail.com>
2025-04-25 21:22:31 +0200

    hero padding

--- src/app/layouts/ModernHero.tsx
+++ src/app/layouts/ModernHero.tsx
@@ -15,7 +15,7 @@ export function ModernHero() {
       style={{ backgroundImage: "url('/images/hero-bg.png')" }}
     >
       <Spotlight />
-      <div className=" pt-12 heroContent p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+      <div className=" heroContent p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           NextPhase <br />
         </h1>
